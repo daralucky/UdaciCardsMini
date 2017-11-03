f6d6b1ef0ddf7fe01136f9177d47d06fa265b1e4
Dara Heng <daraheng.ncdd@gmail.com>
2017-11-03 23:13:51 +0700

    feat: Adding basic navigation: TabNavigation

--- App.js
+++ App.js
@@ -1,13 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { purple, white, red } from './utils/colors';
+import { Constants } from 'expo';
+
+function UdaciStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+}
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+const Dashboard = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Dashboard</Text>
+    </View>
+  );
+};
+
+const Tabs = TabNavigator(
+  {
+    Home: {
+      screen: Home,
+    },
+    Dashboard: {
+      screen: Dashboard,
+    },
+  },
+  {
+    navigationOptions: {
+      header: null,
+    },
+  }
+);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{ flex: 1 }}>
+        <UdaciStatusBar backgroundColor={red} barStyle="light-content" />
+        <Tabs />
       </View>
     );
   }
@@ -17,6 +59,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    flexDirection: 'row', // optional
     alignItems: 'center',
     justifyContent: 'center',
   },
