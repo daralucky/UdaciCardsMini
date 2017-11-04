f856ef486755380cd0b9c9503ee16ed159e97416
Dara Heng <daraheng.ncdd@gmail.com>
2017-11-04 15:40:54 +0700

    feat: Add Get Back navigation feature

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { NavigationActions } from 'react-navigation';
 
 class DeckDetail extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -9,15 +10,45 @@ class DeckDetail extends Component {
       title: `Deck ID: ${deckId}`,
     };
   };
+
+  handlePress = () => {
+    //alert('Hello');
+    this.toHome();
+  };
+
+  toHome = () => {
+    this.props.navigation.dispatch(
+      NavigationActions.back({ key: this.props.navigation.state.key })
+    );
+  };
+
   render() {
     return (
-      <View>
+      <View style={{ flex: 1, alignItems: 'center' }}>
         <Text>
           Deck Detail - deckId: {this.props.navigation.state.params.deckId}
         </Text>
+        <Text>Screen key: {this.props.navigation.state.key}</Text>
+        <TouchableOpacity onPress={this.handlePress}>
+          <Text style={styles.btn}>Go Back</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  btn: {
+    //flex: 1,
+    backgroundColor: '#E53224',
+    padding: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    //borderColor: '#000',
+  },
+});
 
 export default DeckDetail;
